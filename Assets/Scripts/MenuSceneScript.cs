39330ac5197ccc5f78a09e05c4782dcce67f79a0
Bielczy <44232416+Bielczy@users.noreply.github.com>
2019-06-17 12:42:55 +0200

    Add files via upload

---
+++ Assets/Scripts/MenuSceneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuSceneScript : MonoBehaviour
+
+{
+    public Button btnRightHand, btnLeftHand, btnExit;
+    public GameObject WelcomePanel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        WelcomePanel.SetActive(true);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnRightClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnLeftClick()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SampleSceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SampleSceneScript : MonoBehaviour
+{
+    public Button btnBack, btnElbowFlex_Ext_R, btnRotationR, btnFistR,
+        btnWristFlex_Ext_FrontR, btnWristFlex_Ext_SagitR, btnElbowFlex_Ext_L,
+        btnRotationL, btnFistL, btnWristFlex_Ext_FrontL, btnWristFlex_Ext_SagitL;
+    public GameObject ExpositionPanel, WelcomePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBackClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
